387163914d72b0a02b439d6a18e2c5b27c351312
kooozka <jkozanecki@op.pl>
2023-08-29 17:44:59 +0200

    added more html views

---
+++ src/main/java/com/koozka/simpleshop/model/Item.java
@@ -0,0 +1,18 @@
+package com.koozka.simpleshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Item {
+    private String name;
+    private BigDecimal price;
+    private String imgUrl;
+}
