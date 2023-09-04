3c92df91910e47a8351ca08dbbd553001e13ba35
kooozka <jkozanecki@op.pl>
2023-09-04 12:49:05 +0200

    added filter option

--- src/main/java/com/koozka/simpleshop/controller/HomeController.java
+++ src/main/java/com/koozka/simpleshop/controller/HomeController.java
@@ -1,31 +1,54 @@
 package com.koozka.simpleshop.controller;
 
+import com.koozka.simpleshop.model.SportDiscipline;
+import com.koozka.simpleshop.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.koozka.simpleshop.service.CartService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     private final CartService cartService;
+    private final ItemRepository itemRepository;
+    private SportDiscipline selectedDiscipline;
 
     @Autowired
-    public HomeController(CartService cartService) {
+    public HomeController(CartService cartService, ItemRepository itemRepository) {
         this.cartService = cartService;
+        this.itemRepository = itemRepository;
+        selectedDiscipline = null;
     }
 
     @GetMapping("/")
     public String home(Model model) {
+        selectedDiscipline = null;
         model.addAttribute("items", cartService.getAllItems());
+        model.addAttribute("disciplines", SportDiscipline.values());
         return "home";
     }
 
     @GetMapping("/add/{itemId}")
-    public String addItemToCart(@PathVariable("itemId") Integer itemId, Model model) {
+    public String addItemToCart(@PathVariable("itemId") Integer itemId,
+                                @RequestParam(value = "discipline", required = false) String discipline,
+                                Model model) {
         cartService.addItemToCart(itemId);
+        if (selectedDiscipline != null) {
+            return "redirect:/filter?discipline=" + selectedDiscipline;
+        }
         model.addAttribute("items", cartService.getAllItems());
+        model.addAttribute("disciplines", SportDiscipline.values());
+        return "redirect:/";
+    }
+
+    @GetMapping("/filter")
+    public String getFilteredItems(@RequestParam("discipline") String discipline,  Model model) {
+        selectedDiscipline = SportDiscipline.valueOf(discipline);
+        model.addAttribute("items", itemRepository.findAllByDiscipline(selectedDiscipline));
+        model.addAttribute("disciplines", SportDiscipline.values());
         return "home";
     }
 }
--- src/main/java/com/koozka/simpleshop/repository/ItemRepository.java
+++ src/main/java/com/koozka/simpleshop/repository/ItemRepository.java
@@ -1,7 +1,11 @@
 package com.koozka.simpleshop.repository;
 
 import com.koozka.simpleshop.model.Item;
+import com.koozka.simpleshop.model.SportDiscipline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+    List<Item> findAllByDiscipline(SportDiscipline sportDiscipline);
 }
